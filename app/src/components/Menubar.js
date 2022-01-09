f41c45616bc9dd8efebafc22074a1e6d10ae0149
ajaydhiman9611 <dajayhiman9611@gmail.com>
2022-01-09 15:36:56 +0530

    Made some changes in the menubar

--- app/src/components/Menubar.js
+++ app/src/components/Menubar.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BiUser } from "react-icons/bi";
 
 export default function Menubar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3 mb-3 bg-body rounded">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Introduction to Machine Learning</a>
+                <ul className="navbar-nav mb-2 ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link disabled">
+                            <BiUser />&nbsp;&nbsp;
+                            {/* <FaUserSecret /> */}
+                            Hello, Ajay!
+                        </a>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
